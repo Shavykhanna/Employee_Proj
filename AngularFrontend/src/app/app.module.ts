1a8d986b2d2a310768c91fddd54dfdd2a4a87d18
Shavy <shavy@Shavys-MacBook-Pro.local>
2021-07-22 19:56:57 +1000

    Git error resolved

---
+++ AngularFrontend/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { ViewEmployeeComponent } from './components/view-employee/view-employee.component';
+import { AddEmployeeComponent } from './components/add-employee/add-employee.component';
+import { UpdateEmployeeComponent } from './components/update-employee/update-employee.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ViewEmployeeComponent,
+    AddEmployeeComponent,
+    UpdateEmployeeComponent
+  ],
+  imports: [
+    BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
+    RouterModule.forRoot([
+      { path: '', component: ViewEmployeeComponent },
+      { path: 'employee', component: ViewEmployeeComponent },
+      { path: 'employee/add', component: AddEmployeeComponent },
+      { path: 'employee/update/:id', component: UpdateEmployeeComponent }
+    ])
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
